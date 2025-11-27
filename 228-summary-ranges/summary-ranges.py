417cb46f33fc3f4325819bd04424c24123c9555d
NITHYA S <164543102+iamsnithya@users.noreply.github.com>
2025-11-27 10:20:54 +0530

    Time: 4 ms (0.84%) | Memory: 12.4 MB (81.69%) - LeetSync

---
+++ 228-summary-ranges/summary-ranges.py
@@ -0,0 +1,4 @@
+class Solution(object):
+    def summaryRanges(self, nums):
+        return [re.sub('->.*>', '->', '->'.join(`n` for i, n in g))
+            for _, g in itertools.groupby(enumerate(nums), lambda (i, n): n-i)]
