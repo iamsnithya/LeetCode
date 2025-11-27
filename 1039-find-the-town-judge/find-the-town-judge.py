0533a83c42d173502a1adfe3905142322c332348
NITHYA S <164543102+iamsnithya@users.noreply.github.com>
2025-11-27 10:31:53 +0530

    Time: 7 ms (97.78%) | Memory: 16 MB (63.17%) - LeetSync

---
+++ 1039-find-the-town-judge/find-the-town-judge.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def findJudge(self, n, trust):
+        count = [0] * (n + 1)
+        
+        for i, j in trust:
+            count[i] -= 1
+            count[j] += 1
+            
+        for i in range(1, n + 1):
+            if count[i] == n - 1:
+                return i
+            
+        return -1
