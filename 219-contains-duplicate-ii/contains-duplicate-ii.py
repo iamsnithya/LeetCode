1d42dccdc22f3abf68b138844c371ec2140adbb5
NITHYA S <164543102+iamsnithya@users.noreply.github.com>
2025-11-28 07:15:37 +0530

    Time: 39 ms (47.17%) | Memory: 25.1 MB (32.54%) - LeetSync

---
+++ 219-contains-duplicate-ii/contains-duplicate-ii.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def containsNearbyDuplicate(self, nums, k):
+        hset = {}
+        for idx in range(len(nums)):
+            if nums[idx] in hset and abs(idx - hset[nums[idx]]) <= k:
+                return True
+            hset[nums[idx]] = idx
+        return False
+        
